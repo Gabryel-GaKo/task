847ea0a5d62d5396a3fa275a0f34875938ba4bed
Luis Santos <luisfernandosantos504@gmail.com>
2026-06-02 16:04:37 -0300

    integração frontend

--- security.py
+++ security.py
@@ -21,3 +21,10 @@ async def get_current_user(
             headers={"WWW-Authenticate": "Bearer"},
         )
     return user
+
+async def get_current_active_user(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
